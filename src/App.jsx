1cfce632952a3625620f56d9939ea8407150e1e7
junjun932 <1004ji93@gmail.com>
2024-08-22 15:25:30 +0900

    "파일추가"

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import pokemonBg from "./assets/pokedex.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="=App container">
       <div className="header">
